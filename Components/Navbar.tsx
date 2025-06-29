b0bf53193b14c334b7e22af8c9b28ddd9b719f0f
Priyanshu <priyanshusingh.0601@gmai.com>
2025-06-29 21:37:57 +0530

    responsive navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -6,13 +6,17 @@ export default function Navbar() {
   const user = "Tony Stark"
 
   return (
-    <>
-      <div className='px-10 py-3 w-full h-20 flex justify-between items-center rounded-2xl'>
-          <h1 className='text-2xl'>Welcome Back, {user}!</h1>
-        <div className='flex justify-end items-center gap-7'>
-            <Switch/>
-            <Bell size={30} className='hover:cursor-pointer'/>
-            <CircleUser size={30} className='hover:cursor-pointer'/>
+    <>      
+      <div className="px-5 md:px-10 py-3 w-full h-20 flex flex-row items-center rounded-2xl">
+
+        <h1 className="text-sm md:text-2xl pl-6">
+          Welcome Back, {user}!
+        </h1>
+
+        <div className="flex justify-end items-center gap-5 flex-1">
+          <Switch/>
+          <Bell className="hover:cursor-pointer size-6 md:size-8" />
+          <CircleUser className="hover:cursor-pointer size-6 md:size-8 hidden md:block" />
         </div>
       </div>
     </>
